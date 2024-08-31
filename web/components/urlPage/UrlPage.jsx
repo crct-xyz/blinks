1841f1411212d66055d74dc2d04a62b0e097517e
eloi <recycled_loops@hotmail.com>
2024-08-31 18:13:50 +0200

    fix in modal url styles

--- web/components/urlPage/UrlPage.jsx
+++ web/components/urlPage/UrlPage.jsx
@@ -11,11 +11,6 @@ function UrlPage({ address }) {
     return (
         <div className={styles.container}>
             <div className={styles.wrapper}>
-                {/* <div className={styles.text1}>Your link:</div> */}
-                {/* <div className={styles.box}>
-                    <div className={styles.link}>{`https://blinks-ecru.vercel.app/api/action/approve?squad=${address}`}</div>
-                    <div className='copy-button cursor-pointer' data-clipboard-text={`https://blinks-ecru.vercel.app/api/action/approve?squad=${address}`}><FaCopy /></div>
-                </div> */}
                  <a className={styles.span} target="_blank" href={`https://dial.to/?action=solana-action%3Ahttps://blinks-ecru.vercel.app/api/action/approve?squad=${address}`}>Your Link</a>
             </div>
         </div>
